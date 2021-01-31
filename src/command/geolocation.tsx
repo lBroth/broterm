ebdfa6d7064b17a2577a102d3e85937e49449f46
Broth <little.broth@yahoo.it>
2021-01-31 21:16:53 +0100

    geolocation ipify.org

--- src/command/geolocation.tsx
+++ src/command/geolocation.tsx
@@ -1,7 +1,7 @@
 import {CommandObjectType, CommandType} from "./index.d";
 
 function geolocationResponseToString(json:any) {
-	return `Geolocation plugin by geoplugin.net` +
+	return `Geolocation plugin by ipify.org` +
 		Object.keys(json)
 			.map((c) => "\n" + c.replace("geoplugin_", "") + ": " + json[c])
 			.join("") +
@@ -10,8 +10,8 @@ function geolocationResponseToString(json:any) {
 
 async function func() {
 	try {
-		const response = await fetch('http://www.geoplugin.net/json.gp')
-		const json: { geoplugin_request: string } = await response.json()
+		const response = await fetch('https://api.ipify.org?format=json')
+		const json = await response.json()
 		return {result: geolocationResponseToString(json), command: CommandType.GEOLOCATION}
 	} catch(e) {
 		console.error(e);
